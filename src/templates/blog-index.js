71c03290f344eb9c9dc09f06ff5592e9a1d4ce5c
Guillaume Besson <guillaume@besson.co>
2019-03-04 14:19:08 +0100

    Fixed error when supporting a language without posts yet

--- src/templates/blog-index.js
+++ src/templates/blog-index.js
@@ -15,7 +15,7 @@ class BlogIndexTemplate extends React.Component {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title');
     const langKey = this.props.pageContext.langKey;
 
-    const posts = get(this, 'props.data.allMarkdownRemark.edges');
+    const posts = get(this, 'props.data.allMarkdownRemark.edges') || [];
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
